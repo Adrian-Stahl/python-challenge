d380769860c6dec20f1083342212609c3730f6c0
Adrian <carlos.adrian.stahl@gmail.com>
2023-01-14 20:54:23 -0600

    pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,30 +3,70 @@
 
 import csv
 
-with open('budget_data.csv') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',') #by doing this we are setting comma as the sign that separates values.
+with open('/Users/castahl/Desktop/UTSA/Bootcamp/Challenges/python challenge /python-challenge/PyBank/Resources/budget_data.csv') as csvfile:
+    csvreader = csv.reader(csvfile,
+                           delimiter=',')  # by doing this we are setting comma as the sign that separates values.
+    # to read headers
+    header = next(csvreader)
 
-#Since the dataset has a couple of columns named "Date" and "Profit/Losses" we need it to read these columns by creating lists.
-#lets create lists as we need to keep order of these elements.
-    date=[] # for the date column
-    profitloss=[] # for the Profit/Losses column
+    # Since the dataset has a couple of columns named "Date" and "Profit/Losses" we need it to read these columns by creating lists.
+    # lets create lists as we need to keep order of these elements.
+    date = []  # for the date column
+    profitloss = []  # for the Profit/Losses column
 
-#complete VARIABLES according to tasks:
-    months=0
-    average=0
-    total=0
-    month=0
-    differentiaa=0
-    differentiab=0
-    diferenciaa=0
-    diferenciab=0
-    x=0
-    y=0
-    total=0
+    # complete VARIABLES according to tasks:
+    months = 0
+    average = 0
+    total = 0
+    ma = 0
+    differentiaa = 0
+    differentiab = 0
+    diferenciaa = 0
+    diferenciab = 0
+    x = 0
+    y = 0
+    total = 0
 
-    month=0
+    mb = 0
 
-#The total number of months included in the dataset
-    month = len(months)
+    # this is how we let pythons know in which column are the date and earnings
+    for row in csvreader:
+        monthyear = row[0]
+        earnings = row[1]
+        date.append(monthyear)
+        profitloss.append(earnings)
+        # The total number of months included in the dataset
+    ma = len(date)
 
 
+    for x in range (ma-1):
+        average = average + (float(profitloss[x+1]) - float(profitloss[x]))
+        mb = (float(profitloss[x+1]) - float(profitloss[x]))
+
+        if mb < differentiab:
+            differentiab = mb
+            diferenciab = x
+        else:
+            differentiab = differentiab
+
+        if mb > differentiaa:
+            differentiaa = mb
+            diferenciaa = x
+        else:
+            differentiaa = differentiaa
+
+    for y in range (ma):
+        total = total + int(profitloss[y])
+Finalwork=f'\
+    Financial Analysis\n\
+    ----------------------------\n\
+    Total Months:{ma}\n\
+    Total:${total}\n\
+    Average Change: $ {round(average/(ma - 1),2)}\n\
+    Greatest Increase in Profits:{date[diferenciaa+1]} ${int(differentiaa)}\n\
+    Greatest Decrease in Profits: {date[diferenciab + 1]} ${int(differentiab)}\n'
+print(Finalwork)
+
+pybank = open("pybank.txt", 'w')
+pybank.writelines(Finalwork)
+pybank.close()
