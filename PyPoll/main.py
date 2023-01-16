0cba17c4b41213ccc96eb85d6d9f0b281a0e169f
Adrian <carlos.adrian.stahl@gmail.com>
2023-01-15 21:05:41 -0600

    2nd project 2nd day

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,7 @@
 import csv
 
-with open('/Users/castahl/Desktop/UTSA/Bootcamp/Challenges/python challenge /python-challenge/PyPoll/Resources /election_data.csv') as csvfile:
+with open(
+        '/Users/castahl/Desktop/UTSA/Bootcamp/Challenges/python challenge /python-challenge/PyPoll/Resources /election_data.csv') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')  # by doing this we are setting comma as the sign that separates
     # values.
     # to read headers
@@ -18,11 +19,11 @@ with open('/Users/castahl/Desktop/UTSA/Bootcamp/Challenges/python challenge /pyt
     count = 0
     cons = 0
     pros = 0
-    #variables for 2nd,3rd, 4rd, and 5th task
-    alpha= 0
+    # variables for 2nd,3rd, 4rd, and 5th task
+    alpha = 0
     beta = 0
     gamma = 0
-    delta =0
+    delta = 0
 
     # this is how we let python know in which column are the date and earnings
     for row in csvreader:
@@ -36,12 +37,16 @@ with open('/Users/castahl/Desktop/UTSA/Bootcamp/Challenges/python challenge /pyt
 
 # first task is total number of votes cast
 count = len(ballotid)
-print(count) #test and checkpoint
+print(count)  # test and checkpoint
 # so we first need to create a list of different candidates: they will have to go through
 # candidates and placed them on their own list if they are different.
 
-#go through candidate, if different, append to another list called "name"
+# go through candidate, if different, append to another list called "name"
 
-for alpha in range (count-1)
+for alpha in range(count - 1):
+    if candidate[alpha + 1] != candidate[alpha] and candidate[alpha + 1] not in name:
+        name.append(candidate[alpha + 1])
+candidateVotes = len(name)
 
-
+#print test and checkpoint
+print(candidateVotes)#only three candidates in this whole list! good job for today.
