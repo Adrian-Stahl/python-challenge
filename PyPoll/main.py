46401e05a959a76d9018990c51e53c201b465ff1
Adrian <carlos.adrian.stahl@gmail.com>
2023-01-15 19:46:06 -0600

    2nd day 2nd project

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('/Users/castahl/Desktop/UTSA/Bootcamp/Challenges/python challenge/python-challenge/PyPoll/Resources/election_data.csv') as csvfile:
+with open('/Users/castahl/Desktop/UTSA/Bootcamp/Challenges/python challenge /python-challenge/PyPoll/Resources /election_data.csv') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')  # by doing this we are setting comma as the sign that separates
     # values.
     # to read headers
