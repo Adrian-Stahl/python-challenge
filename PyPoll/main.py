74c19cc74d1c0bf9b67760fc7441b36bba8e09d3
Adrian <carlos.adrian.stahl@gmail.com>
2023-01-15 18:11:57 -0600

    2nd project 2nd day

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,3 +3,8 @@ with open('/Users/castahl/Desktop/UTSA/Bootcamp/Challenges/python challenge /pyt
     csvreader = csv.reader(csvfile, delimiter=',')  # by doing this we are setting comma as the sign that separates values.
     # to read headers
     header = next(csvreader)
+
+    #variables (so far I know three for sure)
+    ballotid=[]
+    county=[]
+    candidate=[]
